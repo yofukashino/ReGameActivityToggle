8bdaf1d680cbe3659ad113b7f71b590cb0e515ff
Tharki-god <alokender018@gmail.com>
2023-03-02 19:06:45 +0530

    v1.0.0 compatibility for next plugins

--- src/plaintextPatches.ts
+++ src/plaintextPatches.ts
@@ -5,11 +5,11 @@ export default [
     find: "isCopiedStreakGodlike",
     replacements: [
       {
-        match: /([A-Za-z]\.[A-Za-z]\([A-Za-z],\{default:\(\)=>[A-Za-z]+)(\}\))/,
+        match: /([\w+]\.[\w+]\([\w+],\{default:\(\)=>[\w+]+)(\}\))/,
         replace: `$1,AccountDetails: () => accountDetails$2`,
       },
       {
-        match: /var (.*)=(function.*\}.*\);.*hoveringOnMute:!1)/,
+        match: /var ([\w\d!@#$%^&*)(+=._-]+)=(function.*\}.*\);.*hoveringOnMute:!1)/,
         replace: `var $1,accountDetails;$1=accountDetails=$2`,
       },
     ],
